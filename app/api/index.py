7f29e89ad3c86b348766690b305348fef604d16b
saniameer232-alt <saniameer232@gmail.com>
2026-08-23 04:50:11 -0700

    remove vercel specific configuration files

--- app/api/index.py
+++
@@ -1,8 +0,0 @@
-
-import sys
-import os
-
-# Root directory ko path mein add karna taake 'main.py' ya 'app' easily import ho sakay
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-from main import app
